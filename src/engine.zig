f00a1c4988a904d2a3741d85bf33b7379ee25e74
MartinVacheron <martinvdu92@gmail.com>
2026-01-20 22:11:43 +0100

    added more cubes and rotations

--- src/engine.zig
+++ src/engine.zig
@@ -58,14 +58,70 @@ pub fn setupWindow() void {
 
     // Data
     // zig fmt: off
-    const vertices = [_]f32{
-         // positions      // colors        // texture coords
-         0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0,   // top right
-         0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0,   // bottom right
-        -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom let
-        -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0    // top let 
+    // const vertices = [_]f32{
+    //      // positions      // colors        // texture coords
+    //      0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0,   // top right
+    //      0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0,   // bottom right
+    //     -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom let
+    //     -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0    // top let 
+    // };
+    const vertices = [_]f32 {
+        -0.5, -0.5, -0.5,  0.0, 0.0,
+         0.5, -0.5, -0.5,  1.0, 0.0,
+         0.5,  0.5, -0.5,  1.0, 1.0,
+         0.5,  0.5, -0.5,  1.0, 1.0,
+        -0.5,  0.5, -0.5,  0.0, 1.0,
+        -0.5, -0.5, -0.5,  0.0, 0.0,
+
+        -0.5, -0.5,  0.5,  0.0, 0.0,
+         0.5, -0.5,  0.5,  1.0, 0.0,
+         0.5,  0.5,  0.5,  1.0, 1.0,
+         0.5,  0.5,  0.5,  1.0, 1.0,
+        -0.5,  0.5,  0.5,  0.0, 1.0,
+        -0.5, -0.5,  0.5,  0.0, 0.0,
+
+        -0.5,  0.5,  0.5,  1.0, 0.0,
+        -0.5,  0.5, -0.5,  1.0, 1.0,
+        -0.5, -0.5, -0.5,  0.0, 1.0,
+        -0.5, -0.5, -0.5,  0.0, 1.0,
+        -0.5, -0.5,  0.5,  0.0, 0.0,
+        -0.5,  0.5,  0.5,  1.0, 0.0,
+
+         0.5,  0.5,  0.5,  1.0, 0.0,
+         0.5,  0.5, -0.5,  1.0, 1.0,
+         0.5, -0.5, -0.5,  0.0, 1.0,
+         0.5, -0.5, -0.5,  0.0, 1.0,
+         0.5, -0.5,  0.5,  0.0, 0.0,
+         0.5,  0.5,  0.5,  1.0, 0.0,
+
+        -0.5, -0.5, -0.5,  0.0, 1.0,
+         0.5, -0.5, -0.5,  1.0, 1.0,
+         0.5, -0.5,  0.5,  1.0, 0.0,
+         0.5, -0.5,  0.5,  1.0, 0.0,
+        -0.5, -0.5,  0.5,  0.0, 0.0,
+        -0.5, -0.5, -0.5,  0.0, 1.0,
+
+        -0.5,  0.5, -0.5,  0.0, 1.0,
+         0.5,  0.5, -0.5,  1.0, 1.0,
+         0.5,  0.5,  0.5,  1.0, 0.0,
+         0.5,  0.5,  0.5,  1.0, 0.0,
+        -0.5,  0.5,  0.5,  0.0, 0.0,
+        -0.5,  0.5, -0.5,  0.0, 1.0,
     };
     // zig fmt: on
+
+    const cubes_pos = [_]math.Vec3{
+        math.vec3(0.0, 0.0, 0.0),
+        math.vec3(2.0, 5.0, -15.0),
+        math.vec3(-1.5, -2.2, -2.5),
+        math.vec3(-3.8, -2.0, -12.3),
+        math.vec3(2.4, -0.4, -3.5),
+        math.vec3(-1.7, 3.0, -7.5),
+        math.vec3(1.3, -2.0, -2.5),
+        math.vec3(1.5, 2.0, -2.5),
+        math.vec3(1.5, 0.2, -1.5),
+        math.vec3(-1.3, 1.0, -1.5),
+    };
 
     const indicies = [_]u32{
         0, 1, 3, // first triangle
@@ -102,11 +158,12 @@ pub fn setupWindow() void {
     // We don't want normalization of our data
     // Stride/padding between our data. The array is tightly packed
     // Offset of where the position data begins in the buffer (0 for us)
-    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 8 * @sizeOf(f32), @ptrFromInt(0));
+    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 5 * @sizeOf(f32), @ptrFromInt(0));
     gl.glEnableVertexAttribArray(0);
-    gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 8 * @sizeOf(f32), @ptrFromInt(3 * @sizeOf(f32)));
-    gl.glEnableVertexAttribArray(1);
-    gl.glVertexAttribPointer(2, 2, gl.GL_FLOAT, gl.GL_FALSE, 8 * @sizeOf(f32), @ptrFromInt(6 * @sizeOf(f32)));
+    // gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 8 * @sizeOf(f32), @ptrFromInt(3 * @sizeOf(f32)));
+    // gl.glEnableVertexAttribArray(1);
+    // gl.glVertexAttribPointer(2, 2, gl.GL_FLOAT, gl.GL_FALSE, 8 * @sizeOf(f32), @ptrFromInt(6 * @sizeOf(f32)));
+    gl.glVertexAttribPointer(2, 2, gl.GL_FLOAT, gl.GL_FALSE, 5 * @sizeOf(f32), @ptrFromInt(3 * @sizeOf(f32)));
     gl.glEnableVertexAttribArray(2);
 
     const tex1 = Texture.create("assets/wood_container.jpg", .rgb);
@@ -117,11 +174,15 @@ pub fn setupWindow() void {
     shader_prog.setInt("tex1", 0);
     shader_prog.setInt("tex2", 1);
 
+    gl.glEnable(gl.GL_DEPTH_TEST);
+
     while (glfw.glfwWindowShouldClose(window) == 0) {
         glfw.glfwPollEvents();
 
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, if (wireframe) gl.GL_LINE else gl.GL_FILL);
+
         gl.glClearColor(0.2, 0.3, 0.3, 1.0);
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT);
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
 
         // To tell we use those shaders
         shader_prog.use();
@@ -140,22 +201,38 @@ pub fn setupWindow() void {
         const green: f32 = (@sin(time) / 2.0) + 0.5;
         shader_prog.setFloat("green", green);
 
-        var trans = math.Mat4.identity;
-        trans = trans.mul(.createAngleAxis(math.vec3(0, 0, 1), time));
-        trans = trans.mul(.createTranslationXYZ(0.5, -0.5, 0.0));
-        shader_prog.setMat4f("transform", trans);
+        // We move the camera backward from scene -> +z
+        // But in reality we move the whole scene the opposite direction so -z
+        const view = math.Mat4.createTranslationXYZ(0, 0, -3.0);
+        shader_prog.setMat4f("view", view);
 
-        if (wireframe) {
-            gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE);
-        } else {
-            gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL);
+        // 600 and 800 are our viewport size
+        const projection = math.Mat4.createPerspective(math.toRadians(45.0), 800.0 / 600.0, 0.1, 100.0);
+        shader_prog.setMat4f("projection", projection);
+
+        for (cubes_pos, 0..) |pos, i| {
+            var model = math.Mat4.identity;
+            const angle = 20.0 * @as(f32, @floatFromInt(i));
+
+            // ZLM seems to be in reverse order
+            // Either transpose each matrix
+            // model = model.mul(.createTranslation(pos));
+            // model = model.transpose();
+            // model = model.mul(.createAngleAxis(math.vec3(1.0, 0.3, 0.5), math.toRadians(angle)));
+            // model = model.transpose();
+
+            // Or do in reverse order w/r tutorial but I think it's the correct way, always rotate before translating
+            model = model.mul(.createAngleAxis(math.vec3(1.0, 0.3, 0.5), math.toRadians(angle)));
+            model = model.mul(.createTranslation(pos));
+
+            shader_prog.setMat4f("model", model);
+
+            // Draw
+            gl.glDrawArrays(gl.GL_TRIANGLES, 0, 36);
         }
 
-        // Draw
-        // gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3);
-
         // Draw elements based on the indicies. Second args is number of elems and last is the offset
-        gl.glDrawElements(gl.GL_TRIANGLES, 6, gl.GL_UNSIGNED_INT, @ptrFromInt(0));
+        // gl.glDrawElements(gl.GL_TRIANGLES, 6, gl.GL_UNSIGNED_INT, @ptrFromInt(0));
 
         glfw.glfwSwapBuffers(window);
     }
--- src/vert.glsl
+++ src/vert.glsl
@@ -8,10 +8,12 @@ out vec3 ourColor;
 out vec2 TexCoord;
 
 uniform float green;
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main() {
-    gl_Position = transform * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
     ourColor = vec3(0.0, green, 0.0);
     TexCoord = aTexCoord;
 }
